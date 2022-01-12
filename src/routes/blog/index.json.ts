45051d4be9b3d2121a931c70e799d70cc29bfc03
Domenik Reitzner <domenik@reitzner.at>
2022-01-12 09:41:05 +0100

    add logging

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -13,26 +13,32 @@ const internal = posts.map(post => {
 const date2Number = (string: string) => (new Date(string)).getTime();
 
 export const get: RequestHandler = async () => {
-	// maybe cache them with cloudflare?
-	const devTo = await fetch(
-		'https://dev.to/api/articles?username=dreitzner',
-		{
-			headers: {
-				'api-key': import.meta.env.VITE_DEVTO_API_KEY,
-			}
-		}
-	)
 	let devToJson = [];
-	if(devTo.ok){
-		devToJson = (await devTo.json()).map(article => {
-			return {
-				url: article.url,
-				title: article.title,
-				image: article.cover_image,
-				date: article.created_at,
+	try {
+		// maybe cache them with cloudflare?
+		const devTo = await fetch(
+			'https://dev.to/api/articles?username=dreitzner',
+			{
+				headers: {
+					'api-key': import.meta.env.VITE_DEVTO_API_KEY,
+				}
 			}
-		});
+		)
+		console.log('DevTo Request:', devTo.ok)
+		if(devTo.ok){
+			devToJson = (await devTo.json()).map(article => {
+				return {
+					url: article.url,
+					title: article.title,
+					image: article.cover_image,
+					date: article.created_at,
+				}
+			});
+		}
+	} catch (err) {
+		console.error(err);
 	}
+	
 
 	const body = [
 		...internal,
