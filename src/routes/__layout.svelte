fe0c50b3b12baaae9e5677d5b21b8a324640cb29
Domenik Reitzner <domenik@reitzner.at>
2022-06-01 16:57:02 +0200

    Fix layout server render

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,24 +1,32 @@
+<script context="module">
+	const titleFallback = 'Home';
+
+	export async function load({ url: {href, pathname} }) {
+		const firstUrlSegment = pathname.split('/')[1] || titleFallback;
+		const title = firstUrlSegment.slice(0, 1).toUpperCase() + firstUrlSegment.slice(1);
+		const headerImg = ['Music'].includes(title) ? title : titleFallback;
+		return {
+			props: {
+				title,
+				headerImg,
+				href,
+			}
+		};
+	}
+</script>
+
 <script>
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import { page } from '$app/stores';
-	import { afterNavigate } from '$app/navigation';
-	const titleFallback = 'Home';
-	let firstUrlSegment;
-	let title = '';
-	let headerImg;
-
-	afterNavigate(({ to }) => {
-		firstUrlSegment = to.pathname.split('/')[1] || titleFallback;
-		title = firstUrlSegment.slice(0, 1).toUpperCase() + firstUrlSegment.slice(1);
-		headerImg = ['Music'].includes(title) ? title : titleFallback;
-	});
+	export let title = '';
+	export let headerImg;
+	export let href;
 </script>
 
 <svelte:head>
 	<title>reitzner.at - {title}</title>
 	<meta property="og:title" content="reitzner.at - {title}" />
-	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:url" content={href} />
 	<meta property="og:image" content="https://reitzner.at/img/og/{headerImg}>" />
 </svelte:head>
 
