cc0e8f2d664489e1e8ce6338eeee121749c97999
Domenik Reitzner <domenik@reitzner.at>
2023-06-23 11:01:53 +0200

    Upgrade some dependencies for bug repro 🐛

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: process.env.VERCEL_GIT_COMMIT_REF
